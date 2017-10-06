3dd8d9a048b3f24fd6556dfcd9636f468944fa72
cangqiong <cang19910623@163.com>
2017-10-06 17:27:09 +0800

    初始化

---
+++ doc/schema/DDL.sql
@@ -0,0 +1,24 @@
+USE blog;
+
+DROP TABLE IF EXISTS blog_user;
+
+CREATE TABLE blog_user(
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `user_name` VARCHAR(50) NOT NULL COMMENT '用户名',
+  `password` VARCHAR(150) NOT NULL COMMENT '用户密码',
+  `modify_time` DATETIME  COMMENT '修改时间',
+  `create_time` DATETIME  COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE = INNODB DEFAULT CHARSET=utf8;
+
+DELIMITER $$
+CREATE TRIGGER USER_INSER
+BEFORE INSERT ON blog_user
+FOR EACH ROW BEGIN
+      -- Set the creation date
+       SET new.create_time = NOW();
+
+       -- Set the udpate date
+       SET new.modify_time = NOW();
+END $$
+DELIMITER ;
---
+++ src/test/java/com/cang/blog/myblogdemo/MyBlogDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cang.blog.myblogdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyBlogDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
